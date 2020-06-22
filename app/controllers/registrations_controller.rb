d42ea19e60bad89f7d5d89ce167e115c5db50c0b
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-22 11:32:42 +0100

    fix typo in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,15 +5,15 @@ class RegistrationsController < ApplicationController
       password: params['user']['password']
       password_confirmation: params['user']['password_confirmation']
     ) 
-  end
 
-  if user
-    session[:user_id] = user.id 
-    render json: {
-      status: :created,
-      user: user
-    }
-  else
-    render json: { status: 500 }
+    if user
+      session[:user_id] = user.id 
+      render json: {
+        status: :created,
+        user: user
+      }
+    else
+      render json: { status: 500 }
+    end
   end
 end
