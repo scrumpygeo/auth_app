ea62e1a32fd729806f7968be9894d72ee7836d5e
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-22 09:40:41 +0100

    fix typo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if rails.env == "production"
+if Rails.env == "production"
   Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "some-address-api.herokuapp.com"
 else 
   Rails.application.config.session_store :cookie_store, key: "_auth_app"
