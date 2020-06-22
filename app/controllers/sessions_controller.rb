304ce91fa82b51ab03488f95f08d7286799b6360
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-22 11:18:38 +0100

    add logged_in check and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
   def create 
     user = User
             .find_by(email: params["user"]["email"])
@@ -17,7 +18,21 @@ class SessionsController < ApplicationController
   end
 
   def logged_in 
-    
+    if @current_user
+      render json {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def log_out
+    reset_session
+    render json: { status: 200, logged_out: true }
   end
 
 end
