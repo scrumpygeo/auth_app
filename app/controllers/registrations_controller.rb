f5539ffa16873ac66ad60302fe342406fcc97f6c
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-22 11:38:15 +0100

    fix another typo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < ApplicationController
   def create
     user = User.create!(
       email: params['user']['email'],
-      password: params['user']['password']
+      password: params['user']['password'],
       password_confirmation: params['user']['password_confirmation']
     ) 
 
